89172135e8d93974ba32fb71ca7b76f8fe5f68ac
nozomi <kkk@docomo.ne.jp>
2020-09-27 12:15:49 +0900

    スペルミス解消

--- app/models/user_search.rb
+++ app/models/user_search.rb
@@ -4,6 +4,6 @@ class UserSearch
   attr_accessor :search_name, :search_age
 
   def execuete
-    User.ransack(name_eq: @search_name,age_eq: @seach_age).result
+    User.ransack(name_eq: @search_name,age_eq: @search_age).result
   end  
 end 
